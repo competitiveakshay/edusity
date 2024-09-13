50936c4bea4755590651f92a071b30f1775fdb10
Akshay Gaur <akshay.gaur028@gmail.com>
2024-09-13 13:03:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Components/Hero/Hero"
+import Navbar from "./Components/Navbar/Navbar"
+import Programs from "./Components/Programs/Programs"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Programs/>
+    </div>
+  )
+}
+
+export default App
