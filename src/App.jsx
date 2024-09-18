5a8922c9a9b1892cb9ef38a080aead5502b7e2ed
Akshay Gaur <akshay.gaur028@gmail.com>
2024-09-18 11:02:26 +0530

    title and program done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import Programs from "./Components/Programs/Programs"
+import Title from "./Components/Title/Title"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
-      <Programs/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Programs/>
+      </div>
     </div>
   )
 }
