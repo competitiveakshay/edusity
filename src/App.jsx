45229ca7b8c1293781ac50bab4aff7664b453a35
Akshay Gaur <akshay.gaur028@gmail.com>
2024-09-18 12:38:17 +0530

    gallery and testimonials done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import About from "./Components/About/About"
+import Campus from "./Components/Campus/Campus"
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import Programs from "./Components/Programs/Programs"
+import Testimonials from "./Components/Testimonials/Testimonials"
 import Title from "./Components/Title/Title"
 
 const App = () => {
@@ -11,6 +14,12 @@ const App = () => {
       <div className="container">
         <Title subTitle='Our PROGRAM' title='What We Offer' />
         <Programs/>
+        <About/>
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Student Says' />
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in Touch' />
       </div>
     </div>
   )
