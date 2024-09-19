37b5f6906a904e5636cff3246f060adb9e2c5367
Akshay Gaur <akshay.gaur028@gmail.com>
2024-09-19 18:22:17 +0530

    website done

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,68 @@
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import { useState } from 'react'
+
+const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "79473857-616d-467a-9dde-ef7200a33639");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className='contact'>
+
+      <div className="contact-col">
+        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+        <p>Feel free to reach out to contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+        <ul>
+            <li><img src={mail_icon} alt="" /> akshay.gaur028@gmail.com</li>
+            <li><img src={phone_icon} alt="" />+91-9311211182</li>
+            <li><img src={location_icon} alt="" />India, New Delhi</li>
+        </ul>
+      </div>
+
+      <div className='contact-col'>
+        {/* Web3Forms have been used to get data on email after submit the form */}
+        <form onSubmit={onSubmit}>
+            <label>Your Name</label>
+            <input type="text" name='name' placeholder='Enter your name' required />
+            <label>Your Number</label>
+            <input type="tel" name='phone' placeholder='Enter your Mobile Number' required />
+            <label> Write your message here</label>
+            <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+
+            <button type='submit' className='btn dark-btn'>Submit Now</button>
+
+        </form>
+        <span>{result}</span>
+      </div>  
+
+    </div>
+  )
+}
+
+export default Contact
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { useState } from 'react'
 import { useEffect } from 'react';
+import menu_icon from '../../assets/menu-icon.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -13,17 +15,23 @@ const Navbar = () => {
     })
   },[])
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo' />
-      <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact Us</button></li>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
+        <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link> </li>
+        <li> <Link to='program' smooth={true} offset={-260} duration={500}>Program</Link> </li>
+        <li> <Link to='about' smooth={true} offset={-150} duration={500}>About</Link> </li>
+        <li> <Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link> </li>
+        <li> <Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link> </li>
+        <li className='btn'> <Link to='contact' smooth={true} offset={-260} duration={500} >Contact Us</Link> </li>
       </ul>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
